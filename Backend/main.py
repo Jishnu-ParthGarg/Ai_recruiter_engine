8cf8baf5f90a72cd4b79acdbfad5c9b895953f3a
Jishnu-ParthGarg <gargp6995@gmail.com>
2026-06-26 02:33:33 +0530

    Fix backend imports

--- Backend/main.py
+++ Backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-from Backend.recruiter_engine import get_top_candidates
+from recruiter_engine import get_top_candidates
 
 app = FastAPI(title="AI Recruiter API")
 
